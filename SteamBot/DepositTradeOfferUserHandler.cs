d810a0d767a51e66a42238dfadbbdd6611eab813
Noctisae <fanch.toquer@laposte.net>
2016-04-28 18:37:24 +0200

    test

--- SteamBot/DepositTradeOfferUserHandler.cs
+++ SteamBot/DepositTradeOfferUserHandler.cs
@@ -254,6 +254,7 @@ namespace SteamBot
 				response = JsonConvert.DeserializeObject<JSONBasicResponse> (new StreamReader (request.GetResponse ().GetResponseStream ()).ReadToEnd ());
 			}
 			catch(JsonReaderException e){
+				Console.WriteLine(response);
 				Console.WriteLine (e.ToString());
 				Console.WriteLine (e.Data);
 				Console.WriteLine (e.GetType());
